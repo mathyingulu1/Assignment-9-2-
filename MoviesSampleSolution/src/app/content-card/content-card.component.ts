ebae5d7771806859a181c01b541823b51824a46d
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-02-10 11:58:38 -0500

    Completed assignment 2

--- MoviesSampleSolution/src/app/content-card/content-card.component.ts
+++ MoviesSampleSolution/src/app/content-card/content-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-card',
@@ -6,38 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-card.component.css']
 })
 export class ContentCardComponent implements OnInit {
+  @Input() movie?: Content;
 
   constructor() {
-
-
-    // this.myList.addItem({
-    //   id: 0,
-    //   title: "Inception",
-    //   description: "A movie in a movie",
-    //   creator: "Christopher Noland",
-    //   imgURL: "https://m.media-amazon.com/images/I/611ixoDpRLL._AC_.jpg"
-    // });
-    // this.myList.addItem({
-    //   id: 1,
-    //   title: "Kung Fu Panda",
-    //   description: "Jack black if he was a panda",
-    //   creator: "John Stevenson",
-    //   imgURL: "https://m.media-amazon.com/images/M/MV5BODJkZTZhMWItMDI3Yy00ZWZlLTk4NjQtOTI1ZjU5NjBjZTVjXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg",
-    //   type: "cartoon"
-    // });
-    // this.myList.addItem({
-    //   id: 2,
-    //   title: "Avengers",
-    //   description: "A movie about getting Shawarma",
-    //   creator: "Joss Wheden",
-    //   imgURL: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
-    //   type: "marvel"
-    // });
 
   }
 
 
   ngOnInit(): void {
+  }
+
+  imageClicked(): void {
+    console.log("ID: ", this.movie?.id, ", Title: ", this.movie?.title);
   }
 
 }
--- MoviesSampleSolution/src/app/content-list/content-list.component.ts
+++ MoviesSampleSolution/src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-list',
@@ -7,7 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentListComponent implements OnInit {
 
-  constructor() { }
+  movieList: Content[];
+  constructor() {
+    this.movieList = [{
+      id: 0,
+      title: "Inception",
+      description: "A movie in a movie",
+      creator: "Christopher Noland",
+      imgURL: "https://m.media-amazon.com/images/I/611ixoDpRLL._AC_.jpg",
+      tags: ["movieinamovie", "cool stunts"]
+    }, {
+      id: 1,
+      title: "Kung Fu Panda",
+      description: "Jack black if he was a panda",
+      creator: "John Stevenson",
+      imgURL: "https://m.media-amazon.com/images/M/MV5BODJkZTZhMWItMDI3Yy00ZWZlLTk4NjQtOTI1ZjU5NjBjZTVjXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg",
+      type: "cartoon"
+    }, {
+      id: 2,
+      title: "Avengers",
+      description: "A movie about getting Shawarma",
+      creator: "Joss Wheden",
+      imgURL: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
+      type: "sci-fi",
+      tags: ["lotsOfMainCharacters", "firstofmany"]
+    }, {
+      id: 3,
+      title: "Hocus Pocus",
+      description: "She had great hair in this movie",
+      creator: "Kenny Ortega",
+      imgURL: "",
+      type: "drama"
+    }, {
+      id: 4,
+      title: "Avatar",
+      description: "They were all really tall",
+      creator: "James Cameron",
+      imgURL: "",
+      type: "sci-fi"
+    }, {
+      id: 5,
+      title: "White Tiger",
+      description: "I sure hope there's a tiger in it",
+      creator: "Ramin Bahrani",
+      imgURL: "",
+      type: "drama"
+    }];
+  }
 
   ngOnInit(): void {
   }
