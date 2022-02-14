8f8c911b2d965483f3cf34f06dd7bc017e81dbc8
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-02-14 11:42:06 -0500

    FInished lecture of week 05

--- learning-angular/src/app/highlight.directive.ts
+++ learning-angular/src/app/highlight.directive.ts
@@ -1,13 +1,20 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit {
   @Input() colour?: string;
+
   constructor(private elm: ElementRef) {
+    elm.nativeElement.style.cursor = "pointer";
   }
   ngOnInit(): void {
-    this.elm.nativeElement.style.backgroundColor = this.colour ?? "green";
+  }
+  @HostListener("click") onClick(btn: any) {
+    this.highlightFunction(this.colour);
+  }
+  private highlightFunction(colorValue?: string): void {
+    this.elm.nativeElement.style.backgroundColor = colorValue ?? "green";
   }
 }
