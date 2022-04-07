a2823d7659edd833a557e37a0df86e95eb1612c6
amillerman <amillerman01@stclaircollege.ca>
2022-04-07 11:17:30 -0400

    Installed a default custom theme

--- MoviesSampleSolution/src/app/app.module.ts
+++ MoviesSampleSolution/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataService } from "./services/in-memory-data.service";
 import { ModifyContentComponentComponent } from './modify-content-component/modify-content-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { ModifyContentComponentComponent } from './modify-content-component/modi
       dataEncapsulation: false,
       delay: 3000,
     }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
